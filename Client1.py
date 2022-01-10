f44124bd8b192e0af4071d74230518e8f6c2b6b3
ikmal <ikmalazman65@gmail.com>
2022-01-10 23:41:55 +0000

    Client

--- Client1.py
+++ Client1.py
@@ -2,20 +2,25 @@ import socket
 HOST = "192.168.56.103"
 PORT = 8889
 
+print("Welcome to Speed ROCK, PAPER, SCISSOR\n")
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
         while True:
                data = b""
                while data != b"\n":
                      data = s.recv(1024)
-               move = input('Say Something: ')
+
+               move = input('Rocks, Paper, Scissor or Exit : ')
                s.sendall(str.encode(move))
                data = b""
+
                while not data:
                      data = s.recv(1024)
                opponent = data.decode('utf-8')
                print(type(opponent))
                print("Received:", opponent)
+
                if move == "Rocks":
                        if move == opponent:
                              print( "It's Tie")
@@ -25,6 +30,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                              print("You lose")
                        else:
                              print("Invalid move Rocks")
+
                elif move == 'Paper':
                        if move == opponent:
                              print("It's a Tie")
@@ -34,6 +40,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                              print("You win")
                        else:
                              print("Invalid move")
+
                elif move == 'Scissors':
                        if move == opponent:
                              print("It's a Tie")
@@ -43,5 +50,9 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                              print("You lose")
                        else:
                              print("Invalid move")
+
+               elif move == 'Exit':
+                       print("Thank you for playing Speed Rocks,Paper,Scissor\n")
+                       break
                else:
                    print("Invalid Move")
